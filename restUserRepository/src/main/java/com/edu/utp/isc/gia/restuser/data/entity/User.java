891944013c91a0c433d79e8ccc2294adf42b5338
Michael Rendon <michael.rendon@utp.edu.co>
2020-04-04 00:14:57 -0500

    Agregando dependencias paquetes de config y data

---
+++ restUserRepository/src/main/java/com/edu/utp/isc/gia/restuser/data/entity/User.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edu.utp.isc.gia.restuser.data.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Michael
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "Usuario")
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+  
+}
